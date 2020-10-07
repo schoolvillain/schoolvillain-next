8de0b8e2b2139fa384a68f26ccda7d03efd9313e
kdongha <kdongha1597@gmail.com>
2020-10-08 00:00:10 +0900

    next init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from 'components/MyComponent';
+
+function Page(): JSX.Element {
+    return (
+        <>
+            <div>test</div>
+            <MyComponent />
+        </>
+    );
+}
+
+export default Page;
